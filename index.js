8cb2730f1ad77029626f1f24fa2d1b5728936146
ben richards <lenarichards2234@gmail.com>
2023-09-22 23:05:53 +0100

    adding and removing element done

---
+++ index.js
@@ -0,0 +1,34 @@
+let count = 1;
+
+function submitViaEnter(event) {
+    let keyCode = event.keyCode;
+    const enterKey = "13"
+    if(keyCode == enterKey) {
+        addTask()
+    }
+}
+
+function addTask() {
+    const inputBar = document.getElementById("searchBar")
+    const itemcontainer = document.getElementById("itemContainer")
+
+    if (inputBar.value === '') {
+        return alert("you must type something!");
+    }
+
+    if (count > 12) {
+        return alert("too many tasks");
+    }
+
+    let listItem = document.createElement('li')
+    listItem.innerText = inputBar.value
+    itemcontainer.appendChild(listItem)
+    listItem.onclick = function() {removeTask(listItem)}
+    count++;
+    inputBar.value = ''
+}
+
+function removeTask(listItem) {
+    listItem.remove();
+    count--;
+}
